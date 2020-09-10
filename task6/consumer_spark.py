fec9f7c6c35833e11874c37da411d076d5766d24
nhobbs01 <nickhobbs09@gmail.com>
2020-09-10 10:23:58 +0100

    update

--- task6/consumer_spark.py
+++ task6/consumer_spark.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+import pyspark.sql.functions as F
 # spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4
 
 spark = SparkSession.builder.appName('Weather-api').getOrCreate()
@@ -15,8 +16,11 @@ df = spark.readStream.format('kafka')\
 df_data = df.select(from_json(col("value")\
               .cast("string"), json_schema)\
                     .alias('weather')).selectExpr('weather.*')
+nested_cols = [c[0] for c in df_data.dtypes if c[1][:6] == 'struct']
 
-out = df_data.writeStream.format('console').outputMode('append').start()
+df_clean = df_data.select([F.col('lat'),F.col('lon')]+list(map(lambda x: F.col(x).value.alias(x),nested_cols)))
+
+out = df_clean.writeStream.format('console').outputMode('append').start()
 
 out.awaitTermination()
 
